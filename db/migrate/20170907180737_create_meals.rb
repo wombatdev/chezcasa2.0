26fefc8572a92e3e1724d5280b00de3b38e60301
James <james@team-wombat.com>
2017-09-07 14:32:34 -0400

    updated meals model

--- db/migrate/20170907180737_create_meals.rb
+++ db/migrate/20170907180737_create_meals.rb
@@ -1,8 +1,17 @@
 class CreateMeals < ActiveRecord::Migration[5.1]
-  def change
-    create_table :meals do |t|
-
-      t.timestamps
+    def change
+        create_table :meals do |t|
+            t.string :name, null: false
+            t.string :ingredients, null: false, array: true, default: []
+#            t.string :tags, array: true, default: []
+#            t.string :category, index: true, null: false, array: true, default: []
+            t.decimal :price, null: false, precision: 8, scale: 2
+            t.string :portion, null: false
+            t.integer :quantity, null: false
+            t.integer :available, null: false
+            t.references :chef, index: true, null: false
+            t.text :description, null: false, length: {minimum: 40}
+            t.timestamps null: false
+        end
     end
-  end
 end
