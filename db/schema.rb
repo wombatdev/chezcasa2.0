44b0f348560169111993c9cc847af01f15173a59
James <james@team-wombat.com>
2017-09-07 19:03:31 -0400

    Updated meals controller

---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170907182327) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "tagline", null: false
+    t.integer "zip_code", null: false
+    t.string "photo_url"
+    t.string "bio", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ingredients", default: [], null: false, array: true
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.string "portion", null: false
+    t.integer "quantity", null: false
+    t.integer "available", null: false
+    t.bigint "chef_id", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chef_id"], name: "index_meals_on_chef_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
