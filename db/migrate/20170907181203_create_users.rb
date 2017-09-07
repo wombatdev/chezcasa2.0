f70cd4d85dbdb07686eeaed29fb49ef7a81ddde0
James <james@team-wombat.com>
2017-09-07 14:17:08 -0400

    Updated user model with basics

--- db/migrate/20170907181203_create_users.rb
+++ db/migrate/20170907181203_create_users.rb
@@ -1,8 +1,11 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
+class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
-      t.timestamps
+        t.string :name, null: false
+        t.integer :zip_code, null: false
+        t.string :photo_url
+        t.string :ethnicities, index: true, null: false, array: true, default: []
+        t.timestamps null: false
     end
   end
 end
