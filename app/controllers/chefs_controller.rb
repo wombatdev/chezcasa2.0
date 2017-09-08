bfbd8f09014e2eb1dbdc26569d743abcfe9ee1b5
James <james@team-wombat.com>
2017-09-08 15:46:54 -0400

    Changed chef controller from rails front-end setup to angular front-end setup

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,43 +1,65 @@
 class ChefsController < ApplicationController
 
+    # GET /chefs
     def index
         @chefs = Chef.all
+
+        render json: @chefs
     end
 
+    # GET /chefs/1
     def show
-        @chef = Chef.find(params[:id])
+        # @chef = Chef.find(params[:id])
+
+        render json: @chef
     end
 
-    def new
-        @chef = Chef.new
-    end
 
+    # def new
+    #     @chef = Chef.new
+    # end
+
+    # POST /chefs
     def create
-        @chef = Chef.create!(chef_params)
-        redirect_to @chef
+        @chef = Chef.new(chef_params)
+
+        if @chef.save
+            render json: @chef.to_json, status: :created, location: @chef
+        else
+            render json: @chef.errors, status: :unprocessable_entity
+        end
     end
 
-    def edit
-        @chef = Chef.find(params[:id])
-    end
+    # def edit
+    #     @chef = Chef.find(params[:id])
+    # end
 
+    # PATCH/PUT /chefs/1
     def update
-        @chef = Chef.find(params[:id])
-        @chef.update(chef_params)
-        redirect_to @chef
+        if @chef.update(chef_params)
+            render json: @chef
+        else
+            render json: @chef.errors, status: :unprocessable_entity
+        end
     end
 
+    # DELETE /chefs/1
     def destroy
-        @chef = Chef.find(params[:id])
         @chef.destroy
-        redirect_to chefs_path
     end
 
-    def register_chef
-        @chef = Chef.find(params[:id])
-    end
+    # def register_chef
+    #     @chef = Chef.find(params[:id])
+    # end
 
     private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_chef
+        @chef = Chef.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
     def chef_params
         params.require(:chef).permit(:name, :tagline, :zip_code, :photo_url, :bio)
     end
